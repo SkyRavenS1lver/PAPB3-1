355058cee08f3f7112d2570504243c31baafc4bf
SkyRavenS1lver <revandra.aryo.dwi.krisnandaru@mail.ugm.ac.id>
2023-03-01 13:30:16 +0700

    init commit

--- app/src/main/java/com/example/myapplication/WidgetA.java
+++ app/src/main/java/com/example/myapplication/WidgetA.java
@@ -15,10 +15,12 @@ public class WidgetA extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+//        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_a);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+        // refer ke widget yang dibuat, content dari mana
+        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
